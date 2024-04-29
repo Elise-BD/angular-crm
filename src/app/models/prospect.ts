8fc3030001e323af5c646a7ad7cf097c77656b0d
Aline Daems <daems.aline90@gmail.com>
2024-04-29 13:22:30 +0200

    prospect model

---
+++ src/app/models/prospect.ts
@@ -0,0 +1,26 @@
+export interface Prospect {
+
+  id:number;
+  
+  societe:string; 
+  
+  nom:string; 
+  
+  prenom:string; 
+  
+  email:string; 
+  
+  telephone:string; 
+  
+  ville:string;
+  
+  code_postal:string; 
+  
+  pays:string;
+  
+  adresse:string; 
+  
+  
+
+  
+}
